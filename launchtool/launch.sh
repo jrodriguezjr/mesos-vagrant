bea8ba9abdd3d21bfed65a2975aa447ced884f99
Joe Rodriguez Jr <jrod@rackspace.com>
2016-02-14 18:13:24 -0600

    fix(launchtools): curl and json

--- launchtool/launch.sh
+++ launchtool/launch.sh
@@ -1,1 +1,8 @@
-curl
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    	echo "script takes json file as an argument"
+	exit 1;
+fi
+
+curl -X POST -H "Content-Type: application/json" $(ifdata -pa eth1):8080/v2/apps -d@"$@"
